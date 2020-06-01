0d82f0b2c83e15e72388c662bf2b8cee52dd38c9
chealvin88 <liaoj296@gmail.com>
2020-06-01 13:16:34 +0800

    PLZ let me test

--- init_sin.c
+++ init_sin.c
@@ -15,7 +15,7 @@ int init_sin(float a,float b,float c){
 	for(int i=0;i<N;i++){
 		for(int j=0;j<N;j++){
 			int index = i*N + j;
-			analytic[index] = sin(a*float(i)*dx)*sin(b*float(j)*dx)+c;
+			analytic[index] = sin(a*(float)i*dx)*sin(b*(float)j*dx)+c;
 			density[index]   = -(a*a+b*b)*(analytic[index]-c);
 			if( i==0 || j==0 || i==(N-1) || j==(N-1)){potential[index]=bc;}
                         else{potential[index]=0;}
