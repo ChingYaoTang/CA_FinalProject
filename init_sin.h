cddf9c3571306902bcf53948e109192cbbb1d59f
sandy0216 <b07202025@ntu.edu.tw>
2020-06-04 20:56:53 +0800

    Fix return and input value in init_sin.h

--- init_sin.h
+++ init_sin.h
@@ -4,7 +4,6 @@
 extern float dx, bc;
 extern const int N;
 extern const float L;
-extern double *analytic;
-int init_sin(float a,float b,float c);
+void init_sin( double *analytic, double *potential, double *density, float a,float b, float c) ;
 
 #endif
