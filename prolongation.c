53fbdbdc2a1ed2f4313215fa1827bdfea832810e
ChingYaoTang <410414208@gms.ndhu.edu.tw>
2020-06-04 21:29:44 +0800

    Rectify prolongation.c

--- prolongation.c
+++ prolongation.c
@@ -18,14 +18,14 @@ void prolongation( double *matrix_c, int n_c, double matrix_f) {
 	for( int i=1; i<n_f-1; i++ )
 	for( int j=1; j<n_f-1; j++ ) {
 		matrix_f[ind(i, j, n_f)] = matrix_c[ind(i/2, j/2, n_c)]
-			        	 + ( matrix[ind(i/2+1, j/2, n_c)]
-				  	   + matrix[ind(i/2-1, j/2, n_c)]
-				   	   + matrix[ind(i/2, j/2+1, n_c)]
-				   	   + matrix[ind(i/2, j/2-1, n_c)] )/2
-		                	 + ( matrix[ind(i/2+1, j/2+1, n_c)]
-			 	   	   + matrix[ind(i/2-1, j/2-1, n_c)]
-					   + matrix[ind(i/2+1, j/2-1, n_c)]
-				   	   + matrix[ind(i/2-1, j/2+1, n_c)] )/4;
+			        	 + ( matrix_c[ind(i/2+1, j/2, n_c)]
+				  	   + matrix_c[ind(i/2-1, j/2, n_c)]
+				   	   + matrix_c[ind(i/2, j/2+1, n_c)]
+				   	   + matrix_c[ind(i/2, j/2-1, n_c)] )/2
+		                	 + ( matrix_c[ind(i/2+1, j/2+1, n_c)]
+			 	   	   + matrix_c[ind(i/2-1, j/2-1, n_c)]
+					   + matrix_c[ind(i/2+1, j/2-1, n_c)]
+				   	   + matrix_c[ind(i/2-1, j/2+1, n_c)] )/4;
 	}
 	printf("test of prolongation\n");
 //	print(result,n_);
