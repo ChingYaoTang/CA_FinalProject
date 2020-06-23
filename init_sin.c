45e74dd770fd52b691347d5c62f1132f472cef40
ChingYaoTang <410414208@gms.ndhu.edu.tw>
2020-06-23 18:22:00 +0800

    Add some additional functions for FMG

--- init_sin.c
+++ init_sin.c
@@ -8,7 +8,7 @@ extern const float L;
 extern const double dx;
 extern const int N;
 
-void init_sin( double *analytic, double *potential, double *density, const double kx, const double ky, double bc) {
+void init_sin( double *analytic, double *potential, double *density, const double kx, const double ky, double bc ) {
 	for( int i=0; i<N; i++) {
 		for( int j=0; j<N; j++) {
 			analytic[ind( i, j, N )] = sin( kx*i*dx ) * sin( ky*j*dx ) + bc;
@@ -18,4 +18,12 @@ void init_sin( double *analytic, double *potential, double *density, const doubl
 		}
 	}
 	printf("Using sin test problem, N=%d\n",N);
+}
+
+void init_sin_d( double *density, const double kx, const double ky, double bc, int n ) {
+	double h = L/(n-1);
+	for( int i=0; i<n; i++) 
+	for( int j=0; j<n; j++) {
+		density[ind( i, j, n )]  = -( pow(kx,2) + pow(ky,2) ) * ( sin( kx*i*h ) * sin( ky*j*h ) );
+	}
 }
--- restriction.h
+++ restriction.h
@@ -2,5 +2,6 @@
 #define RESTRICTION_H
 
 void restriction( double *matrix_f, int n_f, double *matrix_c );
+void restriction_half_weighting( double *matrix_f, int n_f, double *matrix_c );
 
 #endif
